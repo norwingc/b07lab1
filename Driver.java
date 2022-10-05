cf0ebf44b9e42b564dc66c0ebd66bc9449fc6be2
Victoria Gadea <victoriagadea@Victorias-MacBook-Pro.local>
2022-10-04 21:59:03 -0500

    lab2

--- Driver.java
+++ Driver.java
@@ -1,16 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Driver { 
- public static void main(String [] args) { 
-  Polynomial p = new Polynomial(); 
-  System.out.println(p.evaluate(3)); 
-  double [] c1 = {6,0,0,5}; 
-  Polynomial p1 = new Polynomial(c1); 
-  double [] c2 = {0,-2,0,0,-9}; 
-  Polynomial p2 = new Polynomial(c2); 
-  Polynomial s = p1.add(p2); 
-  System.out.println("s(0.1) = " + s.evaluate(0.1)); 
-  if(s.hasRoot(1)) 
-   System.out.println("1 is a root of s"); 
+ public static void main(String [] args) throws FileNotFoundException { 
+  Polynomial p = new Polynomial("test.txt"); 
+  
+    Polynomial p1 = new Polynomial("test2.txt"); 
+
+    Polynomial s = p1.multiplyPolynomial(p); 
+
+  System.out.println("p(0.1) = " + p.evaluate(0.1)); 
+  if(s.hasRoot(1))
+  System.out.println("1 is a root of p"); 
   else 
-   System.out.println("1 is not a root of s"); 
+  System.out.println("1 is not a root of p"); 
+
+  try {
+    File myObj = new File("result.txt");
+    if (myObj.createNewFile()) {
+      System.out.println("File created: " + myObj.getName());
+    } else {
+      System.out.println("File already exists.");
+    }
+
+    FileWriter myWriter = new FileWriter("result.txt");
+    myWriter.write("Result: ");
+    for (int i = 0; i < s.coef.length; i++) {
+        myWriter.write(s.coef[i]+"x"+s.exponents[i]);
+    } 
+    myWriter.close();
+  } catch (IOException e) {
+    System.out.println("An error occurred.");
+    e.printStackTrace();
+  }
+
+
  } 
 } 
--- Polynomial.java
+++ Polynomial.java
@@ -1,83 +1,158 @@
+import java.io.File; 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner; 
+
 public class Polynomial {
 	
-	private double[] coef;
-	
-	//sets  the  polynomial  to  zero
-	public Polynomial() {
-		this (new double[0]);
-	}
-	
-	/* takes  an  array  of  double  as  an  argument  and  sets  the 
-	coefficients accordingly */
+	public double[] coef = new double[0];
+	public int[] exponents = new int[0];
 
-	public Polynomial(double[] coef) {
-		 this.coef = new double[coef.length];
-		for (int i = 0; i< coef.length; i++) {
-			this.coef[i] = coef[i];
+	public Polynomial(int polSyze) {
+		this.coef = new double[polSyze];
+		this.exponents = new int[polSyze];
+	   for (int i = 0; i< coef.length; i++) {
+		   this.coef[i] = 0;
+		   this.exponents[i] = 0;
+	   }
+	   
+   }
+	
+	//file reader
+	public String newPolynomialString(String file) throws FileNotFoundException{
+  
+		File myPolymialFile = new File(file);
+	
+		Scanner myReader = new Scanner(myPolymialFile);
+	
+		String data = "";
+	
+		//The polynomial has to be in one line only
+		while (myReader.hasNextLine()) {
+			data = myReader.nextLine();
+		}
+		myReader.close();
+		return data;
+	}
+
+	//returns the list of components (Coef with exponent)
+	public String[] splitPolynomial(String stringPolynomial){
+
+	String[] result = stringPolynomial.split("(?=\\+|\\-)");
+		return result;
+	}
+
+	public Polynomial(String text) throws FileNotFoundException{
+		String textPoly = newPolynomialString(text);
+		String[] polyComponents = splitPolynomial(textPoly);
+		
+		this.coef = new double[polyComponents.length];
+		this.exponents = new int[polyComponents.length];
+		
+		for (int i = 0; i < polyComponents.length; i++){
+			
+			if(polyComponents[i].contains("x")){
+				String[] splitAroundX = polyComponents[i].split("x");
+
+				if(splitAroundX[0]!="" &! splitAroundX[0].equals("-") &! splitAroundX[0].equals("+")  && splitAroundX.length>1){
+					this.coef[i] = Double.parseDouble(splitAroundX[0]);
+				 	this.exponents[i] = Integer.parseInt(splitAroundX[1]);
+				}
+				else if(splitAroundX[0]=="" &! splitAroundX[0].equals("-") &! splitAroundX[0].equals("+") && splitAroundX.length>1 ){
+					this.coef[i] = 1;
+				 	this.exponents[i] = Integer.parseInt(splitAroundX[1]);
+				}
+				else if(splitAroundX[0]!="" && splitAroundX.length<=1 &! splitAroundX[0].equals("-") &! splitAroundX[0].equals("+") ){
+					this.coef[i] = Double.parseDouble(splitAroundX[0]);
+				 	this.exponents[i] = 1;
+				}
+				else{
+					this.coef[i] = 1;
+				 	this.exponents[i] = 1;
+				}
+			}
+			else{
+				this.coef[i] = Double.parseDouble(polyComponents[i]);
+				this.exponents[i] = 0;
+			}
 		}
 		
 	}
-    
+
+	//Creates a ArrayList of all the differemt exponents
+	public ArrayList<Integer> diffExponents(Polynomial second){
+
+		Polynomial first = this;
+		Polynomial sec = second;
+
+		ArrayList<Integer> exponenList = new ArrayList<Integer>();
+
+		for (int i = 0; i < first.exponents.length; i++) {
+			exponenList.add(first.exponents[i]);
+		}
+
+		for (int i = 0; i < sec.exponents.length; i++) {
+			if(!exponenList.contains(sec.exponents[i])){
+				exponenList.add(sec.exponents[i]);
+			}
+		}
+
+		return exponenList;
+	}
+
 	/*takes  one  argument  of  type  Polynomial  and returns the polynomial 
 	 *resulting from adding the calling object and the argument 
 	 */
 	
-	
-	public void setCoef(int index, double coeff) {
-		
-        if (index > coef.length - 1) {
-  
-            double temp[] = coef;
-            coef = new double[index + 1];
-
-            for (int i = 0; i < temp.length; i++) {
-            	coef[i] = temp[i];
-            }
-        }
-        
-        coef[index] = coeff;
-    }
-	
-    public Polynomial add(Polynomial second) {
+    public Polynomial add(Polynomial second) throws FileNotFoundException {
     	
-        Polynomial result = new Polynomial();
-        int len1 = this.coef.length;
-        int len2 = second.coef.length;
-        int len = Math.min(len1, len2);
-        int i;
-        
-        for (i = 0; i < len; i++) {
-            result.setCoef(i, this.coef[i] + second.coef[i]);
-        }
-        while (i < len1) {
-            result.setCoef(i, this.coef[i]);
-            i++;
-        }
-        while (i < len2) {
-            result.setCoef(i, second.coef[i]);
-            i++;
-        }
-        return result;
+		ArrayList<Integer> listOfExponents = diffExponents(second);
+		Polynomial result = new Polynomial(listOfExponents.size());
+
+		for (int i = 0; i < listOfExponents.size(); i++) {
+			for (int j = 0; j < second.exponents.length; j++) {
+				if(second.exponents[j]==listOfExponents.get(i)){
+					result.coef[i] += second.coef[j];
+					result.exponents[i] = second.exponents[j];
+				}
+			}
+			for (int j = 0; j < this.exponents.length; j++) {
+				if(this.exponents[j]==listOfExponents.get(i)){
+					result.coef[i] += this.coef[j];
+					result.exponents[i] = this.exponents[j];
+				}
+			}
+		}
+		return result;
     }
-    
+
 	//evaluates the polynomial 
-	
 	public double evaluate (double x) {
 		double result = 0;
 		for(int i = 0; i < coef.length; i++) {
-		 result += coef[i]*Math.pow(x, i);
+		 result += coef[i]*Math.pow(x, this.exponents[i]);
 		}
-		
 		return result;
 	}
 	
 	//determines whether this value is a root of the polynomial or not
-	
 	public boolean hasRoot(double x) {
 		boolean isRoot = evaluate(x) == 0;
-		
 		return isRoot;
 	}
-	
 
+	public Polynomial multiplyPolynomial(Polynomial second) throws FileNotFoundException{
+
+		Polynomial result = new Polynomial(this.exponents.length>=second.exponents.length?this.exponents.length:second.exponents.length);
+		Polynomial resultMul = new Polynomial(this.exponents.length>=second.exponents.length?this.exponents.length:second.exponents.length);
+
+		for (int i = 0; i < this.coef.length; i++) {
+			for (int j = 0; j < second.coef.length; j++) {
+				result.coef[j] = this.coef[i]*second.coef[j];
+				result.exponents[j] = this.exponents[i]+second.exponents[j];
+			}
+			resultMul = resultMul.add(result);	
+		}
+		return resultMul;
+	}
 }
